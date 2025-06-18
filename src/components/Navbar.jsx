8292e079e79d080789e2e3d2330a7895fe32558b
duddk <duddk@scatterx.io>
2025-06-18 11:03:13 +0900

    navbar 사이 간격 추가

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router';
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav style={{ display: 'flex', gap: '1rem' }}>
       <Link to='/'>Home</Link>
       <Link to='/videos'>Videos</Link>
     </nav>
