7d2a1fbf0f56fcadfe4fa37868b408b231603c84
duddk <duddk@scatterx.io>
2025-07-25 13:36:03 +0900

    react-query 연습

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,12 @@ import { Link } from 'react-router';
 
 export default function Navbar() {
   return (
-    <nav style={{ display: 'flex', gap: '1rem' }}>
-      <Link to='/'>Home</Link>
-      <Link to='/videos'>Videos</Link>
+    <nav style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <div style={{ display: 'flex', gap: '1rem' }}>
+        <Link to='/'>Home</Link>
+        <Link to='/videos'>Videos</Link>
+        <Link to='/posts'>Posts</Link>
+      </div>
     </nav>
   )
 }
